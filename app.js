d77f2ae2efc7718be01579844477073053aa2872
Carlos josé <carlosjsl95@gmail.com>
2017-10-17 21:49:10 -0300

    Configurando o banco de dados

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import express from 'express'
+import datasource from './config/datasource'
+import config from './config/config'
+
 
 const app = express()
+
+app.config = config 
+app.datasource = datasource(app)
+
 app.set('port', 3000)
 
 app.use(bodyParser.json())
---
+++ config/config.js
@@ -0,0 +1,12 @@
+export default {
+    database: 'devlab',
+    username: '',
+    password: '',
+    params: {
+      dialect: 'sqlite',
+      storage: 'devlab.sqlite',
+      define: {
+        underscored: true,
+      },
+    }
+  };
