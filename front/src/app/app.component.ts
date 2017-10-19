38bd4f32b23b502ce7a2cd0362fe786b5d1f52d7
Carlos josé <carlosjsl95@gmail.com>
2017-10-19 11:49:17 -0300

    Implementando o formulario

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import {AppService} from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'app works!';
+  signup= false;
+  formularioLogin: FormGroup;
+  formularioCadastro : FormGroup;
+  appService: AppService;
+
+  constructor(private formBuilder: FormBuilder, _appService: AppService) {
+    this.appService = _appService;
+  }
+
+  onSubmitLogin(){
+    console.log(this.formularioLogin);
+  }
+
+  onSubmitCadastro(){
+    console.log(this.formularioCadastro)
+  }
+
+  ngOnInit() {
+    this.formularioLogin = this.formBuilder.group({
+        email: [null],
+        password: [null]
+    });
+
+    this.formularioCadastro = this.formBuilder.group({
+      name: [null],
+      email: [null],
+      password: [null]
+    });
+  }
+
 }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,20 +1,21 @@
+import { AppService } from './app.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
